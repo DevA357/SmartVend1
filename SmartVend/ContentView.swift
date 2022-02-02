7d4ac23178f1faa5d3ce72cda100090bf91ac065
Devin Allen <tomas0512@gmail.com>
2022-02-02 09:08:14 -0500

    Initial Commit

---
+++ SmartVend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartVend
+//
+//  Created by Devin Allen on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartVend/SmartVendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartVendApp.swift
+//  SmartVend
+//
+//  Created by Devin Allen on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SmartVendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
