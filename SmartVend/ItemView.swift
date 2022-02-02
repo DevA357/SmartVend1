fda83667259f8ba1bde707071ae8bb6706c10b19
Devin Allen <tomas0512@gmail.com>
2022-02-02 11:31:59 -0500

    i made item view

---
+++ SmartVend/ItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemView.swift
+//  SmartVend
+//
+//  Created by Devin Allen on 2/2/22.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemView()
+    }
+}
